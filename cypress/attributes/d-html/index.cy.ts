b73d1e5887c453484c6f20957686e52fa5d7e49f
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-03-26 14:19:02 -0400

    test

--- cypress/attributes/d-html/index.cy.ts
+++ cypress/attributes/d-html/index.cy.ts
@@ -3,6 +3,10 @@ beforeEach(() => {
 });
 
 describe('Attribute d-html test', () => {
+  it('Check attribute is removed', () => {
+    cy.get('h1').should('not.have.attr', 'd-html');
+  });
+
   it('Check the value of the state is inserted into the page', () => {
     cy.get('h1').should('have.html', '<p>Hello World!</p>');
   });
--- cypress/attributes/textContent/index.cy.ts
+++ cypress/attributes/textContent/index.cy.ts
@@ -1,12 +1,8 @@
 beforeEach(() => {
-  cy.visit('cypress/attributes/d-text/index.html');
+  cy.visit('cypress/attributes/textContent/index.html');
 });
 
-describe('Attribute d-text test', () => {
-  it('Check attribute is removed', () => {
-    cy.get('h1').should('not.have.attr', 'd-text');
-  });
-
+describe('textContent test', () => {
   it('Check the value of the state is inserted into the page', () => {
     cy.get('h1').should('have.text', 'Hello World!');
   });
