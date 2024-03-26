34514bf95e112800247a6668e1286ac6f4aaa17f
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-03-26 13:54:24 -0400

    Cypress config

--- TODO.md
+++ TODO.md
@@ -2,10 +2,7 @@
 
 - prevValue and newValue for watcher
 
-- Fixe d-for speed
+- Fixe d-for speed (improve global speed)
 
 - Dont trigger notification if value still the same
 - Test class in state
-- Improve speed ?
-
-- Cypress tests
--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: false
+    specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.ts'
   }
 });
