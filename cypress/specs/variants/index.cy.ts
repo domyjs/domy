1f254040311832ec225ba1a0c8c77382d57b67a4
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-03-31 12:47:20 -0400

    test for variants

--- cypress/specs/variants/index.cy.ts
+++ cypress/specs/variants/index.cy.ts
@@ -5,11 +5,33 @@ beforeEach(() => {
 });
 
 describe('variants test', () => {
-  it('Check the value change one time', () => {
+  it('Once: Check the value change one time', () => {
     cy.get('h1').should('have.text', 'Count: 0');
-    cy.get('button').click();
+    cy.get('#once button').click();
     cy.get('h1').should('have.text', 'Count: 1');
-    cy.get('button').click();
+    cy.get('#once button').click();
     cy.get('h1').should('have.text', 'Count: 1');
+  });
+
+  it('Keydown ENTER: Check the event is triggered only if enter pressed', () => {
+    cy.get('h2').should('have.text', 'Username: ');
+    cy.get('#enter input').type('Yoann');
+    cy.get('h2').should('have.text', 'Username: ');
+    cy.get('#enter input').type('{enter}');
+    cy.get('h2').should('have.text', 'Username: Yoann');
+  });
+
+  it('Click away: Check the event is triggered only if a click is made away from the element', () => {
+    cy.get('modal').should('not.exist');
+
+    cy.get('#away button').click();
+    cy.get('modal').should('exist');
+
+    cy.get('modal p').click();
+    cy.get('modal').click();
+    cy.get('modal').should('exist');
+
+    cy.get('body').click();
+    cy.get('modal').should('not.exist');
   });
 });
