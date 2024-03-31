5a1a17aad13953c1292ef4b0912d66ecc227b5b2
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-03-31 14:03:16 -0400

    Create toRegularFn.ts

---
+++ src/utils/toRegularFn.ts
@@ -0,0 +1,26 @@
+const AsyncFunction = async function () {}.constructor;
+
+/**
+ * Transform an arrow function to a regular function to bind the context
+ * @param arrowFn
+ * @returns
+ */
+export function toRegularFn<T extends (...args: any[]) => any>(arrowFn: T): T {
+  try {
+    const fnString = arrowFn.toString();
+    const match = /(?<isAsync>async)?\s*\((?<params>[^)]*?)\)\s*=>\s*\{?(?<code>[\s\S]*?)\}?$/.exec(
+      fnString
+    );
+    if (match) {
+      const isAsync = match.groups?.isAsync === 'async';
+      const fn = isAsync ? AsyncFunction : Function;
+      const params = match.groups?.params.split(',') ?? [];
+      const code = match.groups!.code;
+      return fn(...params, isAsync ? `return (async () => { ${code} })()` : code) as T;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  return arrowFn;
+}
