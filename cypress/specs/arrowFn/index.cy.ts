1a8a3823189baf2ae1e36484e560306d76ac9b9a
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-03-31 14:06:30 -0400

    test for arrow functions

---
+++ cypress/specs/arrowFn/index.cy.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+
+beforeEach(() => {
+  cy.visit(path.join(__dirname, 'index.html'));
+});
+
+describe('arrowFn', () => {
+  it('Check the app is working', async () => {
+    await new Promise(r => setTimeout(r, 1000));
+
+    cy.get('h1').should('have.text', 'Count: 2');
+
+    cy.get('#dec').click();
+    cy.get('h1').should('have.text', 'Count: 1');
+    cy.get('#dec').click();
+    cy.get('h1').should('have.text', 'Count: 0');
+    cy.get('#dec').click();
+    cy.get('h1').should('have.text', 'Count: 0');
+
+    cy.get('#inc').click();
+    cy.get('h1').should('have.text', 'Count: 1');
+  });
+});
