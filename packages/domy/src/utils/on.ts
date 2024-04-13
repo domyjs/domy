cd8f482d0a28a802b46784c1731c0ee3ece6ea73
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-04-13 16:51:35 -0400

    fixe click away

--- packages/domy/src/utils/on.ts
+++ packages/domy/src/utils/on.ts
@@ -24,6 +24,8 @@ export default function on(props: {
   const options: AddEventListenerOptions = props.options ?? {};
 
   let listener = props.listener;
+  let listenerTarget = props.el;
+
   const { el, eventName, modifiers } = props;
 
   if (modifiers.includes('prevent'))
@@ -38,19 +40,41 @@ export default function on(props: {
     });
   if (modifiers.includes('self'))
     listener = wrapListener(listener, (next, event) => {
-      event.target === el && next(event);
+      event.target === listenerTarget && next(event);
     });
 
   if (modifiers.includes('passive')) options.passive = true;
   if (modifiers.includes('capture')) options.capture = true;
 
+  if (modifiers.includes('away')) {
+    listenerTarget = document.body;
+    listener = wrapListener(listener, (next, event) => {
+      if (event.target !== el && !el.contains(event.target as Node)) {
+        next(event);
+      }
+    });
+  }
+
+  if (modifiers.includes('enter')) {
+    listener = wrapListener(listener, (next, event) => {
+      if ('key' in event && event.key === 'Enter') {
+        next(event);
+      }
+    });
+  }
+
   if (modifiers.includes('once')) {
     listener = wrapListener(listener, (next, event) => {
       next(event);
 
-      el.removeEventListener(eventName, listener, options);
+      listenerTarget.removeEventListener(eventName, listener, options);
     });
   }
 
-  el.addEventListener(eventName, listener, options);
+  // In come case we want to add a listener to an element after a click append
+  // For example d-if render the other attributes after it will be display
+  // So if we click on a button to show the modal then the d-if will render the @click.away
+  // The problem is that the click.away will be catch because the click will be bubble to the body
+  // So to ensure all events are propaged before adding the new event we make it async
+  setTimeout(() => listenerTarget.addEventListener(eventName, listener, options), 0);
 }
