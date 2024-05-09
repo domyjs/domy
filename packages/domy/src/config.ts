5517fdce25302ec5d6e8f688f9b6d446ddd5676c
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-05-09 10:27:45 -0400

    Ensure the configuration is set only one time

--- packages/domy/src/config.ts
+++ packages/domy/src/config.ts
@@ -1,9 +1,18 @@
 import { Config } from './types/Config';
+import { error } from './utils/logs';
 
 let config: Config = {};
+let isConfigSet = false;
 
 export const configuration = {
   setConfig(newConfig: Config) {
+    // We check the app is not already configured
+    if (isConfigSet) {
+      error(new Error(`The configuration can only be set one time.`));
+      return;
+    }
+
+    isConfigSet = true;
     config = newConfig;
   },
   getConfig() {
