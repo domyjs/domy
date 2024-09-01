6fe48145f266318ad1bf92555c38623c22e2a1d7
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-08-31 22:36:34 -0400

    Throttle helper

--- packages/throttle/src/index.ts
+++ packages/throttle/src/index.ts
@@ -1,1 +1,19 @@
-// TODO
+/**
+ * Throttle utility implementation
+ * @param domy
+ * @returns
+ *
+ * @author yoannchb-pro
+ */
+export function $throttle() {
+  return function throttle(fn: Function, limit: number) {
+    let inThrottle = false;
+    return function (this: any, ...args: any[]) {
+      if (!inThrottle) {
+        fn.apply(this, args);
+        inThrottle = true;
+        setTimeout(() => (inThrottle = false), limit);
+      }
+    };
+  };
+}
