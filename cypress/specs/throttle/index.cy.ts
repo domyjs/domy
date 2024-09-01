27d67bd6cdc0903b3ba8aeeaa98f88fbedd898d5
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-09-01 12:05:52 -0400

    throttle test

--- cypress/specs/throttle/index.cy.ts
+++ cypress/specs/throttle/index.cy.ts
@@ -4,8 +4,8 @@ beforeEach(() => {
   cy.visit(path.join(__dirname, 'index.html'));
 });
 
-describe('methods test', () => {
-  it('Check watcher is called', () => {
+describe('Throttle test', () => {
+  it('Check we throttle the function every 500ms', () => {
     cy.get('h1').should('have.text', '0');
 
     cy.get('#inc').click();
