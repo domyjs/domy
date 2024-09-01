918a8a8651de071bc0cc000ea7043729ccc0f434
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-08-31 22:40:19 -0400

    Debounce implementation

--- packages/debounce/src/index.ts
+++ packages/debounce/src/index.ts
@@ -1,1 +1,18 @@
-// TODO
+/**
+ * Debounce utility implementation
+ * @param domy
+ * @returns
+ *
+ * @author yoannchb-pro
+ */
+export function $debounce() {
+  return function (fn: Function, delay: number) {
+    let timeoutId: NodeJS.Timeout;
+    return function (this: any, ...args: any[]) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        fn.apply(this, args);
+      }, delay);
+    };
+  };
+}
