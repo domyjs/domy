49d92b171a403a14398bf5876609fa289e2a8d54
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-09-03 19:38:24 -0400

    collapse plugin implementation+tests

---
+++ cypress/specs/collapse/index.cy.ts
@@ -0,0 +1,34 @@
+import path from 'path';
+
+beforeEach(() => {
+  cy.visit(path.join(__dirname, 'index.html'));
+});
+
+describe('Collapse directive', () => {
+  it('should initially show all content', () => {
+    // Assert that content is not collapsed initially
+    cy.get('#c1').should('not.be.visible');
+    cy.get('#c2').should('be.visible'); // We have a default height so we can see 10px of it
+    cy.get('#c2').should('have.css', 'height', '10px');
+    cy.get('#c3').should('not.be.visible');
+  });
+
+  it('should collapse and expand the content on button click', () => {
+    // Click to collapse content
+    cy.get('button').click();
+
+    // Check that content is collapsed
+    cy.get('#c1').should('be.visible');
+    cy.get('#c2').should('be.visible');
+    cy.get('#c2').should('not.have.css', 'height', '10px');
+    cy.get('#c3').should('have.css', 'height', '100px'); // height 100 for c3
+
+    // Click again to expand content
+    cy.get('button').click();
+
+    // Check that content is expanded again
+    cy.get('#c1').should('not.be.visible');
+    cy.get('#c2').should('have.css', 'height', '10px');
+    cy.get('#c3').should('not.be.visible');
+  });
+});
--- packages/collapse/src/index.ts
+++ packages/collapse/src/index.ts
@@ -1,10 +1,63 @@
-// TODO
-const collapsePlugin = [
-  {
-    type: 'attribute',
-    name: 'd-collapse',
-    fn: () => {}
-  }
-];
+import type DOMY from '@domyjs/core/src';
+import type { DomyDirectiveHelper } from '@domyjs/core/src/types/Domy';
 
-export default collapsePlugin;
+type CollapseSettings = {
+  height?: number;
+  defaultHeight?: number;
+  transition?: string;
+};
+
+const SETTINGS_ATTRIBUTE = 'd-collapse-settings';
+
+/**
+ * Collapse directive
+ * It will hide the element by setting the height to 0px
+ * When it have to be show the height will go back to normal height with a transition
+ * @param domy
+ * @returns
+ *
+ * @author yoannchb-pro
+ */
+export function collapsePlugin(domy: DomyDirectiveHelper) {
+  const el = domy.el as HTMLElement;
+
+  // We deep render the element first to get his initial height
+  domy.deepRender({
+    element: el,
+    state: domy.state,
+    scopedNodeData: domy.scopedNodeData,
+    byPassAttributes: [domy.attr.name]
+  });
+
+  const settingsAttr = el.getAttribute(SETTINGS_ATTRIBUTE);
+  const settings: CollapseSettings = settingsAttr
+    ? domy.evaluateWithoutListening(settingsAttr)
+    : {};
+  el.removeAttribute(SETTINGS_ATTRIBUTE);
+
+  const initialHeight = settings.height ?? el.getBoundingClientRect().height;
+
+  let isInitialised = false;
+  el.style.overflowY = 'hidden';
+
+  domy.effect(() => {
+    const isShow = domy.evaluate(domy.attr.value);
+
+    if (isInitialised) el.style.transition = settings.transition ?? 'height 250ms ease-out';
+
+    if (isShow) {
+      el.style.height = `${initialHeight}px`;
+    } else {
+      el.style.height = `${settings.defaultHeight ?? 0}px`;
+    }
+
+    isInitialised = true;
+  });
+}
+
+document.addEventListener('domy:ready', event => {
+  const { detail: DOMYOBJ } = event as CustomEvent<typeof DOMY>;
+  DOMYOBJ.plugin(domyPluginSetter => {
+    domyPluginSetter.directive('collapse', collapsePlugin);
+  });
+});
