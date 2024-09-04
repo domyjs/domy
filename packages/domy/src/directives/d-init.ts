84b169f34ca824bb6614b3929e7a2a218f7cc010
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-09-04 13:18:07 -0400

    queueJob for init directive

--- packages/domy/src/directives/d-init.ts
+++ packages/domy/src/directives/d-init.ts
@@ -2,12 +2,13 @@ import { DomyDirectiveHelper } from '../types/Domy';
 
 /**
  * d-init implementation
- * Allow to execute some javascript/a function when an element is initialised by domy
+ * Allow to execute some javascript/a function when an element is initialised by domy for the first time
+ * Example: <div d-init="console.log('My div is rendering')">...</div>
  * @param domy
  *
  * @author yoannchb-pro
  */
 export function dInitImplementation(domy: DomyDirectiveHelper) {
   const value = domy.evaluateWithoutListening(domy.attr.value);
-  if (typeof value === 'function') value();
+  if (typeof value === 'function') domy.queueJob(() => value());
 }
