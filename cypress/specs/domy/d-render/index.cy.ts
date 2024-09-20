2a0e5341edf10d68a69f6d7c0e7211c6c9e0d3cc
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-09-20 11:29:23 -0400

    d-render finished

--- cypress/specs/domy/d-render/index.cy.ts
+++ cypress/specs/domy/d-render/index.cy.ts
@@ -5,16 +5,43 @@ beforeEach(() => {
 });
 
 describe('d-render test', () => {
+  it('Check we have just the button at first', () => {
+    cy.get('p').should('not.exist');
+    cy.get('#inc').should('not.exist');
+    cy.get('#dec').should('not.exist');
+  });
+
+  it('Check we can add the count and make it disapear again', () => {
+    cy.get('p').should('not.exist');
+    cy.get('#inc').should('not.exist');
+    cy.get('#dec').should('not.exist');
+
+    cy.get('#renderCount').click();
+
+    cy.get('p').should('exist');
+    cy.get('#inc').should('exist');
+    cy.get('#dec').should('exist');
+
+    cy.get('#renderCount').click();
+
+    cy.get('p').should('not.exist');
+    cy.get('#inc').should('not.exist');
+    cy.get('#dec').should('not.exist');
+  });
+
   it('should display the initial count', () => {
+    cy.get('#renderCount').click();
     cy.get('p').should('have.text', 'Count: 0');
   });
 
   it('should increment the count when "+" button is clicked', () => {
+    cy.get('#renderCount').click();
     cy.get('#inc').click();
     cy.get('p').should('have.text', 'Count: 1');
   });
 
   it('should decrement the count when "-" button is clicked', () => {
+    cy.get('#renderCount').click();
     cy.get('#inc').click();
     cy.get('#inc').click();
 
--- packages/domy/src/directives/d-render.ts
+++ packages/domy/src/directives/d-render.ts
@@ -18,19 +18,33 @@ import { DomyDirectiveHelper, DomyDirectiveReturn } from '../types/Domy';
  * @author yoannchb-pro
  */
 export function dRenderImplementation(domy: DomyDirectiveHelper): DomyDirectiveReturn {
-  const fragment = new DocumentFragment();
+  const parent = domy.el.parentNode as Element;
+  const parentChilds = Array.from(parent.childNodes);
+
+  let el = domy.el;
+  let hasBeenRemove = false;
+  let lastRenderedChilds: Element[] = [];
 
   domy.effect(() => {
-    const elements = domy.evaluateWithoutListening(domy.attr.value);
+    const fragment = new DocumentFragment();
+    const elements = domy.evaluate(domy.attr.value);
+    const isArray = Array.isArray(elements);
 
-    if (!elements) return;
+    // Handle the case we don't have any element(s) to render
+    if (!elements || (isArray && elements.length === 0)) {
+      el.remove();
+      hasBeenRemove = true;
+      return;
+    }
 
-    if (Array.isArray(elements)) {
+    // Append the child(s) to the fragment
+    if (isArray) {
       for (const element of elements) {
         fragment.appendChild(element);
       }
     } else fragment.appendChild(elements);
 
+    // Render the childs
     for (const child of fragment.childNodes) {
       domy.deepRender({
         element: child as Element,
@@ -38,7 +52,24 @@ export function dRenderImplementation(domy: DomyDirectiveHelper): DomyDirectiveR
       });
     }
 
-    domy.utils.replaceElement(domy.el, fragment);
+    // We restore the element if the childrens change and it have been remove
+    if (hasBeenRemove) {
+      const indexToInsert = domy.utils.findElementIndex(parentChilds, domy.el);
+      domy.utils.restoreElement(parent, el, indexToInsert);
+      hasBeenRemove = false;
+    }
+
+    // Copy the fragment child before adding them to the dom because the fragment don't keep them
+    const childsToRender = Array.from(fragment.childNodes) as Element[];
+
+    el.replaceWith(fragment);
+
+    // We remove the last rendered childs
+    for (const element of lastRenderedChilds) {
+      element.remove();
+    }
+    el = childsToRender[0]; // We will replace the first rendered children with the next render
+    lastRenderedChilds = childsToRender;
   });
 
   return { skipChildsRendering: true };
