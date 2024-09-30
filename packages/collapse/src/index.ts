f1c1536b4a1e3cedfbeb29a9781b5122941c0b93
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2024-09-30 13:55:42 -0400

    fixed bugs and better implementation for collapse

--- packages/collapse/src/index.ts
+++ packages/collapse/src/index.ts
@@ -1,5 +1,5 @@
 import type DOMY from '@domyjs/core/src';
-import type { DomyDirectiveHelper } from '@domyjs/core/src/types/Domy';
+import type { DomyDirectiveHelper, DomyDirectiveReturn } from '@domyjs/core/src/types/Domy';
 
 type CollapseSettings = {
   height?: number;
@@ -18,20 +18,21 @@ const SETTINGS_ATTRIBUTE = 'd-collapse-settings';
  *
  * @author yoannchb-pro
  */
-export function collapsePlugin(domy: DomyDirectiveHelper) {
+export function collapsePlugin(domy: DomyDirectiveHelper): DomyDirectiveReturn {
   const el = domy.el as HTMLElement;
+
+  // We get the settings first to ensure it will not throw an error in the following deep render
+  const settingsAttr = el.getAttribute(SETTINGS_ATTRIBUTE);
+  const settings: CollapseSettings = settingsAttr
+    ? domy.evaluateWithoutListening(settingsAttr)
+    : {};
+  el.removeAttribute(SETTINGS_ATTRIBUTE);
 
   // We deep render the element first to get his initial height
   domy.deepRender({
     element: el,
     scopedNodeData: domy.scopedNodeData
   });
-
-  const settingsAttr = el.getAttribute(SETTINGS_ATTRIBUTE);
-  const settings: CollapseSettings = settingsAttr
-    ? domy.evaluateWithoutListening(settingsAttr)
-    : {};
-  el.removeAttribute(SETTINGS_ATTRIBUTE);
 
   const initialHeight = settings.height ?? el.getBoundingClientRect().height;
 
@@ -51,11 +52,18 @@ export function collapsePlugin(domy: DomyDirectiveHelper) {
 
     isInitialised = true;
   });
+
+  return { skipChildsRendering: true, skipOtherAttributesRendering: true };
 }
 
 document.addEventListener('domy:ready', event => {
   const { detail: DOMYOBJ } = event as CustomEvent<typeof DOMY>;
   DOMYOBJ.plugin(domyPluginSetter => {
     domyPluginSetter.directive('collapse', collapsePlugin);
+    domyPluginSetter.directive('collapse-settings', () => {
+      throw new Error(`The directive "collapse-settings" as to be use with "collapse" directive.`);
+    });
+
+    domyPluginSetter.prioritise(['collapse', 'collapse-settings']);
   });
 });
