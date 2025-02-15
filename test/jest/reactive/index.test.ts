e9c5399670f42d49fa97672df93b6e30273e62b7
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2025-02-15 16:12:13 -0500

    Update index.test.ts

--- test/jest/reactive/index.test.ts
+++ test/jest/reactive/index.test.ts
@@ -11,7 +11,8 @@ import {
   matchPath,
   registerName,
   isSignal,
-  isReactive
+  isReactive,
+  trackDeps
 } from '../../../packages/reactive/dist';
 
 describe('Reactive System Tests', () => {
@@ -61,6 +62,143 @@ describe('Reactive System Tests', () => {
 
     // Stop watching changes
     unEffect();
+  });
+
+  it('should track dependencies', () => {
+    const mockGlobalWatch = jest.fn();
+    const mockEffect = jest.fn();
+    const mockWatch = jest.fn();
+
+    let todo: { name: string } | null = null;
+    let count: { value: number } | null = null;
+
+    const deps = trackDeps(() => {
+      todo = reactive({ name: 'Yoann' });
+      count = signal(0);
+
+      globalWatch({ type: 'onSet', fn: mockGlobalWatch });
+      watch({ type: 'onSet', fn: mockWatch }, () => [count, todo]);
+      watchEffect(() => {
+        if (typeof count?.value === 'number' && typeof todo?.name === 'string') {
+          mockEffect();
+        }
+      });
+    });
+
+    expect(deps.length).toBe(5);
+    expect(deps[0].type).toBe('reactive_variable_creation');
+    expect(deps[1].type).toBe('reactive_variable_creation');
+    expect(deps[2].type).toBe('global_watcher');
+    expect(deps[3].type).toBe('watcher');
+    expect(deps[4].type).toBe('effect');
+
+    expect(mockEffect).toHaveBeenCalled();
+    expect(mockGlobalWatch).not.toHaveBeenCalled();
+    expect(mockWatch).not.toHaveBeenCalled();
+
+    mockEffect.mockReset();
+
+    if (todo) (todo as { name: string }).name = 'Pierre';
+    expect(mockEffect).toHaveBeenCalled();
+    expect(mockGlobalWatch).toHaveBeenCalled();
+    expect(mockWatch).toHaveBeenCalled();
+
+    mockEffect.mockReset();
+    mockGlobalWatch.mockReset();
+    mockWatch.mockReset();
+
+    for (const dep of deps) {
+      if (dep.type === 'effect') dep.uneffect();
+      if (dep.type === 'global_watcher') dep.removeGlobalWatcher();
+      if (dep.type === 'watcher') dep.unwatch();
+    }
+
+    if (todo) (todo as { name: string }).name = 'Will';
+    expect(mockEffect).not.toHaveBeenCalled();
+    expect(mockGlobalWatch).not.toHaveBeenCalled();
+    expect(mockWatch).not.toHaveBeenCalled();
+  });
+
+  it('should no listening to dependencies of an other effect inside the current', () => {
+    const todo = reactive({ name: 'Yoann' });
+    const mockEffect = jest.fn();
+    const mockInsideEffect = jest.fn();
+
+    let unEffectInside: (() => void) | null = null;
+    const unEffect = watchEffect(() => {
+      unEffectInside = watchEffect(() => {
+        if (todo.name) {
+          mockInsideEffect();
+        }
+      });
+      mockEffect();
+    });
+
+    expect(mockEffect).toHaveBeenCalled();
+    expect(mockInsideEffect).toHaveBeenCalled();
+
+    mockEffect.mockReset();
+    mockInsideEffect.mockReset();
+    todo.name = 'New Name';
+    expect(mockEffect).not.toHaveBeenCalled();
+    expect(mockInsideEffect).toHaveBeenCalled();
+
+    // Stop watching changes
+    unEffect();
+    if (unEffectInside) (unEffectInside as () => void)();
+
+    mockInsideEffect.mockReset();
+    todo.name = 'new Name 3';
+    expect(mockInsideEffect).not.toHaveBeenCalled();
+  });
+
+  it('Check watcher and effects are removed when the variables become unReactive', () => {
+    const todo1 = reactive({ name: 'Yoann' });
+    const todo2 = reactive({ name: 'Pierre' });
+
+    const mockGlobalWatch = jest.fn();
+    const mockEffect = jest.fn();
+    const mockWatch = jest.fn();
+
+    const removeGlobalWatch = globalWatch({ type: 'onSet', fn: mockGlobalWatch });
+    watch({ type: 'onSet', fn: mockWatch }, () => [todo1.name, todo2.name]);
+    watchEffect(() => {
+      if (todo1.name) {
+        mockEffect();
+      }
+      if (todo2.name) {
+        mockEffect();
+      }
+    });
+
+    expect(mockEffect).toHaveBeenCalled();
+    expect(mockGlobalWatch).not.toHaveBeenCalled();
+    expect(mockWatch).not.toHaveBeenCalled();
+
+    mockEffect.mockReset();
+
+    unReactive(todo1);
+
+    todo1.name = 'New Name';
+    todo2.name = 'New Name';
+    expect(mockEffect).toHaveBeenCalled();
+    expect(mockGlobalWatch).toHaveBeenCalled();
+    expect(mockWatch).toHaveBeenCalled();
+
+    mockGlobalWatch.mockReset();
+    mockEffect.mockReset();
+    mockWatch.mockReset();
+
+    unReactive(todo2);
+
+    todo1.name = 'Another Name';
+    todo2.name = 'Another Name';
+
+    expect(mockEffect).not.toHaveBeenCalled();
+    expect(mockGlobalWatch).not.toHaveBeenCalled();
+    expect(mockWatch).not.toHaveBeenCalled();
+
+    removeGlobalWatch();
   });
 
   it('should trigger effect when reactive data changes', () => {
