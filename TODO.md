5e11a4d00bdb8ba76ea34c87e29d9a9b0e666486
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2025-03-19 21:28:28 -0400

    fixed test for d-for update

--- TODO.md
+++ TODO.md
@@ -1,7 +1,5 @@
 # TODO
 
-- Fixe d-for index
-
 - Router beforeEach/afterEach/isActiveClass/params/replace/redirect/no path and tests
 - store (data/methods), anchor, intersect, mask
 
--- test/cypress/specs/domy/d-key/index.cy.ts
+++ test/cypress/specs/domy/d-key/index.cy.ts
@@ -7,27 +7,27 @@ beforeEach(() => {
 describe('Attribute d-key test', () => {
   it('Check we render correctly the list', () => {
     // Vérifie qu'il y a 2 éléments initialement
-    cy.get('div > p').should('have.length', 2);
+    cy.get('p').should('have.length', 2);
 
     // Vérifie les textes des deux premiers éléments
-    cy.get('div > p').eq(0).should('have.text', 'Hello World1');
-    cy.get('div > p').eq(1).should('have.text', 'Hello World2');
+    cy.get('p').eq(0).should('have.text', 'Hello World1');
+    cy.get('p').eq(1).should('have.text', 'Hello World2');
   });
 
   it('Check the key is applied correctly and elements are not recreated', () => {
-    cy.get('div > p')
+    cy.get('p')
       .eq(0)
       .then(firstElementBefore => {
-        cy.get('div > p')
+        cy.get('p')
           .eq(1)
           .then(secondElementBefore => {
             cy.get('button').click();
-            cy.get('div > p').should('have.length', 3);
+            cy.get('p').should('have.length', 3);
 
-            cy.get('div > p')
+            cy.get('p')
               .eq(0)
               .then(firstElementAfter => {
-                cy.get('div > p')
+                cy.get('p')
                   .eq(1)
                   .then(secondElementAfter => {
                     expect(firstElementBefore[0]).to.equal(firstElementAfter[0]);
