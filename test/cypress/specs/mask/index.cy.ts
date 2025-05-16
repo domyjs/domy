143e6167bb15f27ec9da0f58fa7bfa31b9b27cef
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2025-05-16 11:36:33 -0400

    fixed lint

--- test/cypress/specs/mask/index.cy.ts
+++ test/cypress/specs/mask/index.cy.ts
@@ -6,22 +6,28 @@ beforeEach(() => {
 
 describe('Mask test', () => {
   it('applies static mask for date input', () => {
-    cy.get('#date').type('12122024').should('have.value', '12/12/2024');
+    cy.get('#date').type('12122024');
+    cy.get('#date').should('have.value', '12/12/2024');
   });
 
   it('applies dynamic mask for French phone starting with 33', () => {
-    cy.get('#dynamic').type('33601020304').should('have.value', '33 6 01 02 03 04');
+    cy.get('#dynamic').type('33601020304');
+    cy.get('#dynamic').should('have.value', '33 6 01 02 03 04');
   });
 
   it('applies dynamic mask for Canadian phone not starting with 33', () => {
-    cy.get('#dynamic').clear().type('5141234567').should('have.value', '5 141 234 567');
+    cy.get('#dynamic').clear();
+    cy.get('#dynamic').type('5141234567');
+    cy.get('#dynamic').should('have.value', '5 141 234 567');
   });
 
   it('applies money mask correctly', () => {
-    cy.get('#money').type('1234567.8').blur().should('have.value', '1,234,567.8');
+    cy.get('#money').type('1234567.8').blur();
+    cy.get('#money').should('have.value', '1,234,567.8');
   });
 
   it('applies static mask for letter+number', () => {
-    cy.get('#letters').type('AC1234').should('have.value', 'AC 1234');
+    cy.get('#letters').type('AC1234');
+    cy.get('#letters').should('have.value', 'AC 1234');
   });
 });
