68a4f331276615ab5a02fa642d23f119ef743217
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2025-05-18 13:19:44 -0400

    doc

--- TODO.md
+++ TODO.md
@@ -3,7 +3,6 @@
 - Improve d-for perf and watchEffect perf
 
 - Router beforeEach/afterEach/replace/redirect and tests
-- store (data/methods)
 
 - Documentation
 - Auto release ...
--- packages/docs/src/components/Demo/index.js
+++ packages/docs/src/components/Demo/index.js
@@ -1,22 +1,19 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import DOMY from '@site/static/index.js';
 import styles from './styles.module.css';
 
-const Demo = ({ code, domy }) => {
+if (!window.DOMY) window.DOMY = DOMY;
+
+const Demo = ({ code, domy, components }) => {
   const ref = useRef(null);
 
   useEffect(() => {
-    if (ref.current) {
-      ref.current.innerHTML = code;
-      DOMY.createApp(domy, ref.current);
-    }
+    if (!ref.current) return;
+    ref.current.innerHTML = code;
+    DOMY.createApp(domy).components(components?.()).mount(ref.current);
   }, []);
 
-  return (
-    <>
-      <div className={styles.demo} ref={ref} />
-    </>
-  );
+  return <div className={styles.demo} ref={ref} />;
 };
 
 export default Demo;
