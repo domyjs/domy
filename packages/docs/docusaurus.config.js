b394fb94f2df5c457c5129947f3f360647147310
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2025-05-30 11:15:32 -0400

    homepage docs

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -107,6 +107,10 @@ const config = {
               {
                 label: 'GitHub',
                 href: 'https://github.com/yoannchb-pro/domy'
+              },
+              {
+                label: 'BuyMeACoffee',
+                href: 'https://buymeacoffee.com/yoannchbpro'
               }
             ]
           }
