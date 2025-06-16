2cde4c3f4ae9091d34aac7de7887372deabfe9b0
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2025-06-16 19:47:39 -0400

    fixed baseUrl

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   url: 'https://domyjs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/domy/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
